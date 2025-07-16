f48981c9ff79ee8fc76c1ca9418b73bea39b6937
kizaki <dzaky.adani@gmail.com>
2025-07-16 16:07:49 +0700

    Fix Facility Script

---
+++ Assets/Game/Scripts/Runtime/Managers/FacilityManager.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class FacilityManager : MonoBehaviour
+{
+    [SerializeField] private Button magicShovelButton;
+    [SerializeField] private FacilityDatabaseSO facilityDatabase;
+
+    public FacilityDatabaseSO FacilityDatabase => facilityDatabase;
+
+    private Dictionary<string, float> lastUsedTime = new Dictionary<string, float>();
+
+    private void Awake()
+    {
+        ServiceLocator.Register(this);
+        SaveSystem.TryPurchaseFacility(facilityDatabase.GetFacility("F1"));
+        magicShovelButton.onClick.AddListener(() => UseFacility("F1"));
+    }
+
+    public FacilityDataSO GetFacilityByID(string facilityID)
+    {
+        return facilityDatabase.GetFacility(facilityID);
+    }
+
+    public List<FacilityDataSO> GetAllFacilities() => facilityDatabase.allFacilities;
+
+    public bool CanUseFacility(string facilityID)
+    {
+        if (!SaveSystem.PlayerConfig.HasFacility(facilityID))
+        {
+            Debug.LogWarning($"Facility {facilityID} is not owned.");
+            return false;
+        }
+
+        var facility = GetFacilityByID(facilityID);
+        if (facility == null) return false;
+
+        if (!lastUsedTime.ContainsKey(facilityID))
+            return true;
+
+        float elapsed = Time.time - lastUsedTime[facilityID];
+        Debug.Log(elapsed >= facility.cooldownSeconds);
+        return elapsed >= facility.cooldownSeconds;
+    }
+
+    public bool UseFacility(string facilityID)
+    {
+
+        if (!CanUseFacility(facilityID)) return false;
+
+        var facility = GetFacilityByID(facilityID);
+        if (facility == null) return false;
+
+        lastUsedTime[facilityID] = Time.time;
+
+        switch (facility.facilityID)
+        {
+            case "F1":
+                StartCoroutine(UseMagicShovel());
+                break;
+
+            // Add more cases like:
+            // case "HealingStation":
+            //     StartCoroutine(UseHealingStation());
+            //     break;
+
+            default:
+                Debug.LogWarning($"Facility '{facility.facilityID}' not implemented.");
+                break;
+        }
+
+        return true;
+    }
+    private System.Collections.IEnumerator UseMagicShovel()
+    {
+        var monsterManager = ServiceLocator.Get<MonsterManager>();
+        var poops = new List<PoopController>(monsterManager.activePoops);
+
+        foreach (var poop in poops)
+        {
+            if (poop != null && poop.gameObject.activeInHierarchy)
+            {
+                poop.OnCollected(); // or monsterManager.DespawnToPool(poop.gameObject)
+                yield return new WaitForSeconds(0.2f); // Add a delay for effect
+            }
+        }
+
+        Debug.Log("Magic Shovel used to clean all poop in the current area.");
+    }
+}
