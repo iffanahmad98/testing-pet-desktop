8ccb5745429110801e3504da5bc92107202e9641
Cahyo Ayomi / Zhio <97262169+ayomicahyoa@users.noreply.github.com>
2025-10-02 05:54:44 +0700

    time keeper, monster animation and shop adjustment to buy any animal

--- Assets/Game/Scripts/Runtime/UI/Monster/MonsterCardUI.cs
+++ Assets/Game/Scripts/Runtime/UI/Monster/MonsterCardUI.cs
@@ -5,8 +5,7 @@ using Spine.Unity;
 
 public class MonsterCardUI : MonoBehaviour
 {
-    [Header("UI References")]
-    public Image monsterIcon;
+    [Header("UI References")] public Image monsterIcon;
     public TMP_Text monsterNameText;
     public TMP_Text priceText;
     public Button selectButton;
@@ -15,13 +14,12 @@ public class MonsterCardUI : MonoBehaviour
     public SkeletonGraphic monsterGraphic;
     public Material monsterMaterial; // Optional: For custom material effects
 
-    [Header("Data")]
-    public ItemDataSO monsterItemData;
+    [Header("Data")] public ItemDataSO monsterItemData;
 
     private bool isSelected = false;
 
     public System.Action<MonsterCardUI> OnSelected; // Called when select button clicked
-    public System.Action<MonsterCardUI> OnBuy;      // Called when buy button clicked
+    public System.Action<MonsterCardUI> OnBuy; // Called when buy button clicked
 
     private void Start()
     {
@@ -53,6 +51,7 @@ public class MonsterCardUI : MonoBehaviour
             monsterIcon.gameObject.SetActive(true);
             monsterIcon.sprite = data.itemImgs[0];
         }
+
         monsterNameText.text = data.itemName;
         priceText.text = data.price.ToString();
         SetSelected(false);
@@ -84,6 +83,11 @@ public class MonsterCardUI : MonoBehaviour
                 int _random = Random.Range(0, 2);
                 string randomAnim = _random == 0 ? "itching" : "jumping";
 
+                if (!HasAnimation(randomAnim))
+                {
+                    randomAnim = "jumping";
+                }
+
                 // Play random animation once, then queue idle to loop
                 monsterGraphic.AnimationState.SetAnimation(0, randomAnim, false);
                 monsterGraphic.AnimationState.AddAnimation(0, "idle", true, 0f);
@@ -94,6 +98,11 @@ public class MonsterCardUI : MonoBehaviour
                 monsterGraphic.AnimationState.SetAnimation(0, "idle", true);
             }
         }
+    }
+
+    bool HasAnimation(string animName)
+    {
+        return monsterGraphic.Skeleton.Data.FindAnimation(animName) != null;
     }
 
     public bool IsSelected()
