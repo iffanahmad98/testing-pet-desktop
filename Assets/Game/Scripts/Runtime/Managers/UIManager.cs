d49507d37223dbeaa2fe27c1291f567bc6a24405
Aryadi21 <aryadiperwiras@gmail.com>
2026-01-27 08:29:51 +0700

    - Panel collections dan setting sudah muncul di depan sewaktu tombolnya ditekan

--- Assets/Game/Scripts/Runtime/Managers/UIManager.cs
+++ Assets/Game/Scripts/Runtime/Managers/UIManager.cs
@@ -249,6 +249,7 @@ public class UIManager : MonoBehaviour
             canvasGroup.blocksRaycasts = false;
             canvasGroup.alpha = 0f;
             rect.localScale = Vector3.one;
+            panel.transform.SetAsLastSibling();
 
             MonsterManager.instance.audio.PlaySFX("button_click");
 
