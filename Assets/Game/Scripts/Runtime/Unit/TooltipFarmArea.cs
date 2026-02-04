bbfcd71c488436f7a0fee217a28d9507b2da9923
ElvanDewangga <elvandewangga@gmail.com>
2026-02-04 19:20:51 +0700

    Fix error tool tip farm area

--- Assets/Game/Scripts/Runtime/Unit/TooltipFarmArea.cs
+++ Assets/Game/Scripts/Runtime/Unit/TooltipFarmArea.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
 
 public class TooltipFarmArea : MonoBehaviour {
     [SerializeField] GameObject [] fakeFarmAreaOpened;
     [SerializeField] GameObject [] fakeFarmAreaLocked;    
-    
-    void Start () {
-        playerConfig = SaveSystem.PlayerConfig;
-    }
 
     void RefreshFakeFarmArea () {
-        List <int> idFarms = PlantManager.Instance.GetFarmAreaIdsPurchased ();
+        List <int> idFarms = MagicalGarden.Farm.PlantManager.Instance.GetFarmAreaIdsPurchased ();
 
         int x = 0;
         for (x=0; x < fakeFarmAreaLocked.Length; x++) {
