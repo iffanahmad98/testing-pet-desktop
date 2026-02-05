227fcbd3b2093c4c9fc67d8567e07e758927c24e
Daffa Hanif <daffa03ap@gmail.com>
2026-02-05 22:31:48 +0700

    Farm Shop Panel & fix bug send to plain pop up button

--- Assets/Game/Scripts/Runtime/Farm/Inventory/InventoryUISendToPlains.cs
+++ Assets/Game/Scripts/Runtime/Farm/Inventory/InventoryUISendToPlains.cs
@@ -7,36 +7,44 @@ namespace MagicalGarden.Inventory
         [SerializeField] InventoryUI inventoryUI;
         [SerializeField] Button sendPlainsButton;
         [SerializeField] Image sendPlainsImage;
-        [Header ("Data")]
-        PlayerConfig playerConfig; 
-        void Start () {
+        [Header("Data")]
+        PlayerConfig playerConfig;
+        void Start()
+        {
             playerConfig = SaveSystem.PlayerConfig;
-            sendPlainsButton.onClick.AddListener (SendPlaint);
+            sendPlainsButton.onClick.AddListener(SendPlaint);
         }
 
         #region EventListener
-        public void StartPlains (UITabManager uiTabManager) { // UITabManager.cs
-            uiTabManager.AddEventClick (OnSendPlainsImage);
+        public void StartPlains(UITabManager uiTabManager)
+        { // UITabManager.cs
+            uiTabManager.AddEventClick(OnSendPlainsImage);
         }
         #endregion
 
-        void SendPlaint () {
-            foreach (InventoryItemCell cell in inventoryUI.GetSlotList ()) {
-            ItemDataSO itemDataSO = cell.GetItemDataSO ();
-                int amount = playerConfig.GetItemFarmHarvestAmount (itemDataSO.itemID);
-                playerConfig.AddItem (itemDataSO.itemID, itemDataSO.category, amount);
-                playerConfig.ClearItemFarmHarvest (itemDataSO.itemID);
+        void SendPlaint()
+        {
+            foreach (InventoryItemCell cell in inventoryUI.GetSlotList())
+            {
+                ItemDataSO itemDataSO = cell.GetItemDataSO();
+                int amount = playerConfig.GetItemFarmHarvestAmount(itemDataSO.itemID);
+                playerConfig.AddItem(itemDataSO.itemID, itemDataSO.category, amount);
+                playerConfig.ClearItemFarmHarvest(itemDataSO.itemID);
             }
-            SaveSystem.SaveAll ();
-            inventoryUI.RefreshUI ();
-            ServiceLocator.Get<ItemInventoryUI> ().StartPopulateAllInventoriesWhenOpen();
+            SaveSystem.SaveAll();
+            inventoryUI.RefreshUI();
+            ServiceLocator.Get<ItemInventoryUI>().StartPopulateAllInventoriesWhenOpen();
         }
 
-        public void OnSendPlainsImage (int value) {
-            if (value == 1) {
-            sendPlainsImage.gameObject.SetActive (true);
-            } else {
-                sendPlainsImage.gameObject.SetActive (false);
+        public void OnSendPlainsImage(int value)
+        {
+            if (value == 1)
+            {
+                sendPlainsImage.gameObject.SetActive(true);
+            }
+            else
+            {
+                sendPlainsImage.gameObject.SetActive(false);
             }
         }
 
