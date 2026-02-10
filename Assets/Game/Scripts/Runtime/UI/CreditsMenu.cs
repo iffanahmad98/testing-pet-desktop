b0c8662f5b8f49764dfe5f0840701ea4af0ac86d
Faris Pradiatma <39221506+SashimiEnjoyer@users.noreply.github.com>
2026-02-10 14:56:34 +0700

    Apply transition to pomodoro UI and change transition in credits panel

--- Assets/Game/Scripts/Runtime/UI/CreditsMenu.cs
+++ Assets/Game/Scripts/Runtime/UI/CreditsMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CreditsMenu : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup cGroup;
     [SerializeField] private TMP_Text versionTxt;
 
     private void Start()
@@ -13,18 +14,16 @@ public class CreditsMenu : MonoBehaviour
 
     public void CloseCreditsPanel()
     {
-        Sequence seq = DOTween.Sequence();
-
-        _ =seq.AppendCallback(() =>
+        MonsterManager.instance.audio.PlaySFX("menu_close");
+        cGroup.DOFade(0f, 0.2f).SetEase(Ease.InQuad).OnComplete(() =>
         {
-            transform.DOPunchScale(Vector3.one * 0.1f, 0.2f);
+            gameObject.SetActive(false);
         });
-        seq.AppendInterval(0.2f);
-        seq.AppendCallback(() => { gameObject.SetActive(false); });
     }
 
     public void GoToDiscordLink() => Application.OpenURL("https://discord.gg/cpBTNMeU");
     public void GoToWebsiteLink() => Application.OpenURL("https://petalpals.com/");
     public void GoToTwitterLink() => Application.OpenURL("https://x.com/petalpalsid");
     public void GoToYoutubeLink() => Application.OpenURL("https://www.youtube.com/@ScriptSmelterS");
+    public void PlayClickAudio()=> MonsterManager.instance.audio.PlaySFX("button_click");
 }
